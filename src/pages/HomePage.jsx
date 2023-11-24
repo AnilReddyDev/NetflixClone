f51e16a32db1142a72c969540ec9af38658722b2
Anil Reddy <anilreddy7137@gmail.com>
2023-11-24 15:23:34 +0530

    added netflix content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../App.css'
 export default function HomePage() {
-  const netflixContent = [
+  const netflixSeriesContent = [
     {
       title: "Stranger Things",
       img: "https://fossbytes.com/wp-content/uploads/2022/06/The-Buzz-Paper-1.jpeg",
@@ -32,6 +32,362 @@ export default function HomePage() {
       img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?size=690:388",
       id: "006"
     },
+    {
+      title: "Breaking Bad",
+      img: "https://www.cnet.com/a/img/resize/31d9798922a9fd0bc47854e1fc8e7b226f2e5e3d/hub/2012/10/16/28212967-cbf2-11e2-9a4a-0291187b029a/breaking-bad-header.jpg?auto=webp&width=1200",
+      id: "007"
+    },
+    {
+      title: "Peaky Blinders",
+      img: "https://media-mcetv.ouest-france.fr/wp-content/uploads/2022/01/netflix-cillian-murphy-promet-22du-lourd22-pour-peaky-blinders-saison-6-.jpeg",
+      id: "008"
+    },
+    {
+      title: "kaala Paani",
+      img: "https://www.cineblues.com//uploads/article/kp-teaser-01-rgb-9-x-16-002-20230919234448856.jpg",
+      id: "009"
+    },
+    {
+      title: "The Railway man",
+      img: "https://www.koimoi.com/wp-content/new-galleries/2023/09/yrf-netflix-set-multi-year-creative-partnership-starting-with-the-railway-men-001.jpg",
+      id: "010"
+    },
+    {
+      title: "Daredevil",
+      img: "https://c4.wallpaperflare.com/wallpaper/335/411/593/daredevil-charlie-cox-netflix-wallpaper-preview.jpg",
+      id: "011"
+    },
+    {
+      title: "Narcos",
+      img: "https://brief.promax.org/images/uploads/5723/narcosposter_cube__twocolumncontent.jpg",
+      id: "012"
+    },
+    {
+      title: "Top Boy",
+      img: "https://occ.a.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABW2FXyezQ3TFpHWepXLPVHX-zDV5S2WyNGbEPzNmrDVFlVyDcGz4eWg6KbA8TCO8TVROeic2fPpHqa0MIG6-AV7YrUSfG2ovJvDVtgGfEta5nvCodR4dhQGzKLyqdf7AXdAwnh3kcB1HI-C5LV00tSHJE0BMDHp4pBo8VQ6rgUNsdVnxw4kVKECWEZ9T7WxXQBHrB6lt_deQink2kTtQPS8L4vAhCx_hESNK9z0v_z4AArKNrV-alOLxbkvhuSelOrUn1bspb4yahVkRb7M_bDh5NhSuqkEC6XQeRVHYYEYQ-9PUhiV_.jpg?r=0c2",
+      id: "013"
+    },
+    {
+      title: "Our Planet",
+      img: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABTX11bs4mpJt2lUANAOLnmTVnxfVJjnlKkYAvFi9Hf5_bKo6OegZz0bTSW0p8tPGzG8GO82bvHiaUHKIlcTwASIXlQmn77DL8r-hgWb2OAtpCpvRJVLixFcUD8UxkydTCq2MWA.jpg?r=e29",
+      id: "014"
+    },
+    {
+      title: "After Life",
+      img: "https://i.pinimg.com/originals/f1/82/43/f18243b1fd2d5ee7f3c46a96983d7879.jpg",
+      id: "015"
+    },
+    {
+      title: "Sex Education",
+      img: "https://www.hindustantimes.com/ht-img/img/2023/08/31/1600x900/Screenshot_2023-08-31_235519_1693506340978_1693506363085.png",
+      id: "016"
+    },
+  ]
+
+  const netflixActionMovies = [
+    {
+      title:"The Night Comes for Us",
+      img:"",
+      id:"001"
+    },
+    {
+      title:"Extraction",
+      img:"",
+      id:"002"
+    },
+    {
+      title:"The Old Guard",
+      img:"",
+      id:"003"
+    },
+    {
+      title:"Triple Frontier",
+      img:"",
+      id:"004"
+    },
+    {
+      title:"Wheelman",
+      img:"",
+      id:"005"
+    },
+    {
+      title:"Polar",
+      img:"",
+      id:"006"
+    },
+    {
+      title:"Bright",
+      img:"",
+      id:"007"
+    },
+    {
+      title:"Spectral",
+      img:"",
+      id:"008"
+    },
+    {
+      title:"Lost Bullet",
+      img:"",
+      id:"009"
+    },
+    {
+      title:"Kate",
+      img:"",
+      id:"010"
+    },
+    {
+      title:"Spenser Confidential",
+      img:"",
+      id:"011"
+    },
+    {
+      title:"6 Underground",
+      img:"",
+      id:"012"
+    },
+    {
+      title:"Gunpowder Milkshake",
+      img:"",
+      id:"014"
+    },
+    {
+      title:"Anon",
+      img:"",
+      id:"015"
+    },
+  ]
+
+  const netflixRomanceMovie = [
+    {
+      title:"Cinema Paradiso",
+      img:"",
+      id:"001"
+    },
+    {
+      title:"Amelie",
+      img:"",
+      id:"002"
+    },
+    {
+      title:"Annie Hall",
+      img:"",
+      id:"003"
+    },
+    {
+      title:"Titanic",
+      img:"",
+      id:"004"
+    },
+    {
+      title:"Days of Heaven",
+      img:"",
+      id:"005"
+    },
+    {
+      title:"Manhattan",
+      img:"",
+      id:"006"
+    },
+    {
+      title:"Blue is the Warmest Color",
+      img:"",
+      id:"007"
+    },
+    {
+      title:"BrokeBack Mountain",
+      img:"",
+      id:"008"
+    },
+    {
+      title:"Sense And Sensibility",
+      img:"",
+      id:"009"
+    },
+    {
+      title:"Love Actually",
+      img:"",
+      id:"010"
+    },
+    {
+      title:"From Here to Eternity",
+      img:"",
+      id:"011"
+    },
+    {
+      title:"Fallen Angels",
+      img:"",
+      id:"012"
+    },
+    {
+      title:"Weekend",
+      img:"",
+      id:"013"
+    },
+    {
+      title:"The English Patient",
+      img:"",
+      id:"014"
+    },
+    {
+      title:"Jane Eyre",
+      img:"",
+      id:"015"
+    },
+  ]
+
+  const netflixHorrorContent = [
+    {
+      title:"the Ritual",
+      img:"",
+      id:"001"
+    },
+    {
+      title:"The Good neighbor",
+      img:"",
+      id:"002"
+    },
+    {
+      title:"The Conference",
+      img:"",
+      id:"003"
+    },
+    {
+      title:"The Unholy",
+      img:"",
+      id:"004"
+    },
+    {
+      title:"Evil Dead",
+      img:"",
+      id:"005"
+    },
+    {
+      title:"Sister Death",
+      img:"",
+      id:"006"
+    },
+    {
+      title:"Insidious the Red Door",
+      img:"",
+      id:"007"
+    },
+    {
+      title:"The Black Phone",
+      img:"",
+      id:"008"
+    },
+    {
+      title:"Annabelle",
+      img:"",
+      id:"009"
+    },
+    {
+      title:"Babbe The possession",
+      img:"",
+      id:"010"
+    },
+    {
+      title:"Exorcist The Beginning",
+      img:"",
+      id:"011"
+    },
+    {
+      title:"No One Gets Out Alive",
+      img:"",
+      id:"012"
+    },
+    {
+      title:"The Conjuring",
+      img:"",
+      id:"013"
+    },
+    {
+      title:"Virupaksha",
+      img:"",
+      id:"014"
+    },
+    {
+      title:"Mirrors",
+      img:"",
+      id:"015"
+    },
+  ]
+  const netflixSciFanstasy = [
+    {
+      title:"Jurassic World Camp Cretaceous",
+      img:"",
+      id:"001"
+    },
+    {
+      title:"One Piece",
+      img:"",
+      id:"002"
+    },
+    {
+      title:"Ragnarok",
+      img:"",
+      id:"003"
+    },
+    {
+      title:"Lost in Space",
+      img:"",
+      id:"004"
+    },
+    {
+      title:"Alice in BorderLand",
+      img:"",
+      id:"005"
+    },
+    {
+      title:"Wednesday",
+      img:"",
+      id:"006"
+    },
+    {
+      title:"Raising Dion",
+      img:"",
+      id:"007"
+    },
+    {
+      title:"Manifest",
+      img:"",
+      id:"008"
+    },
+    {
+      title:"The Witcher",
+      img:"",
+      id:"009"
+    },
+    {
+      title:"Sweet Tooth",
+      img:"",
+      id:"010"
+    },
+    {
+      title:"Znation",
+      img:"",
+      id:"011"
+    },
+    {
+      title:"Locke & Key",
+      img:"",
+      id:"012"
+    },
+    {
+      title:"Another Life",
+      img:"",
+      id:"013"
+    },
+    {
+      title:"Fantasy Island",
+      img:"",
+      id:"014"
+    },
+    {
+      title:"Shadow and Bone",
+      img:"",
+      id:"015"
+    },
   ]
   return (
     <div className='w-full h-fit bg-gray-900'>
@@ -41,10 +397,10 @@ export default function HomePage() {
       <div className='HPboxCon  relative w-full h-auto py-5 flex flex-col gap-5 left-0 '>
         <div className='px-10'>
           <h1 className='font-semibold text-2xl text-white'>Continue watching</h1>
-          <div className='w-full flex  overflow-x-auto overflow-y-visible gap-2'>
-            {netflixContent.map((items) => { // continue from here
-              return <div className=" w-64 h-48 flex-none hover:scale-y-125 hover:scale-x-150 bg-pink-200" key={items.id}>
-                <img src={items.img} alt={items.title} className='w-full h-full  z-20 transition-all ease-in' />
+          <div className='w-full flex  overflow-x-auto  gap-2'>
+            {netflixSeriesContent.map((items) => { // continue from here
+              return <div className=" w-72 h-48 flex-none  bg-pink-200" key={items.id}>
+                <img src={items.img} alt={items.title} className='w-full h-full overflow-hidden  z-20 hover:scale-125 transition-all ease-in' />
               </div>
             })}
           </div>
